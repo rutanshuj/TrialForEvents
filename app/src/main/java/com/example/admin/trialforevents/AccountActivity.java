5bbe7019bacd766f69d5c00d05007317a626a0ee
rutanshuj <rjhaveri41@gmail.com>
2017-03-18 23:31:13 +0530

    GoogleSignInComplete

--- app/src/main/java/com/example/admin/trialforevents/AccountActivity.java
+++ app/src/main/java/com/example/admin/trialforevents/AccountActivity.java
@@ -6,12 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class AccountActivity extends AppCompatActivity {
     Button mLogOut;
     private FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleApiClient;
     private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
@@ -31,9 +37,25 @@ public class AccountActivity extends AppCompatActivity {
             }
         };
 
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getApplicationContext())
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Toast.makeText(AccountActivity.this, "You got an Error", Toast.LENGTH_SHORT).show();
+                    }
+                }).addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
         mLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient);
                 mAuth.signOut();
             }
         });
